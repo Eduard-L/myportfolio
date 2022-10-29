2fc11f3d755dc87dd5a869b84b144aea6e624515
Eduard-L <lwqtbdwrd@gmail.com>
2022-10-29 23:13:22 +0300

    fix:portfolio items

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -13,6 +13,8 @@ export function Portfolio({ isOnViewPort, setIsOnViewPort }) {
     const { isVisible } = useIsInVeiwPort(portfolio, 0.5);
     const thirtyPercentVisibility = useIsInVeiwPort(portfolio, 0.3);
     const [scrollPosition, setScrollPosition] = useState(0)
+    const [scroll, setScroll] = useState(0)
+
 
     useEffect(() => {
         setIsOnViewPort(isVisible);
@@ -23,7 +25,9 @@ export function Portfolio({ isOnViewPort, setIsOnViewPort }) {
     }, [isVisible, isAnimationWorked, thirtyPercentVisibility.isVisible])
 
 
-    // let scrollPosition = 0;
+
+
+
 
     useEffect(() => {
         if (scrollPosition >= 1140) {
@@ -38,22 +42,30 @@ export function Portfolio({ isOnViewPort, setIsOnViewPort }) {
         })
 
     }, [scrollPosition])
-    function handleScrollForward() {
-        setScrollPosition(container.current.scrollLeft + 400)
+    function handleScrollForward(e) {
+
+        setScrollPosition(container.current.scrollLeft + 500)
 
 
 
     }
     function handleScrollBack() {
-        setScrollPosition(container.current.scrollLeft - 400)
+        setScrollPosition(container.current.scrollLeft - 500)
+
 
     }
+
+
     return (
         <section ref={portfolio} className='portfolio'>
             <div className={`animation-wrapper ${isAnimationWorked && 'animation-wrapper_animatied'}`}>
                 <h2 className='portfolio__title'>Portfolio</h2>
-                <ul ref={container} className='portfolio__list snaps-inline'>
-                    <li><button onClick={handleScrollBack} ref={preBtn} type='button' className={`portfolio__btn portfolio__btn_type_pre ${scrollPosition <= 200 && 'portfolio__btn_not-visible'} `}></button></li>
+                <ul onScroll={() => setScroll(container.current.scrollLeft)} ref={container} className='portfolio__list snaps-inline'>
+                    {
+                        (scrollPosition > 200) && window.innerWidth > 700 &&
+                        <li><button onClick={handleScrollBack} ref={preBtn} type='button' className={`portfolio__btn portfolio__btn_type_pre  `}></button></li>
+                    }
+
                     <PortfolioCard imgSrc={newsExplorer} titleText='News Explorer' subtitleText='Find the latest news!' type='news-explorer' codeLink='https://github.com/Eduard-L/news-explorer-frontend' projectLink='https://news-searcher.students.nomoredomainssbs.ru/' />
                     <PortfolioCard imgSrc={aroundUsGif} titleText='Around Us' subtitleText='Make your own profile!' type='around-us' codeLink='https://github.com/Eduard-L/react-around-api-full' projectLink='https://around-us.students.nomoredomainssbs.ru' />
                     <PortfolioCard imgSrc={imageSeracher} titleText='Image Searcher' subtitleText='Find you image!' type='image-searcher' codeLink='https://github.com/Eduard-L/react-image-searcher' projectLink='https://eduard-l.github.io/react-image-searcher/' />
@@ -65,8 +77,12 @@ export function Portfolio({ isOnViewPort, setIsOnViewPort }) {
                             <a href="https://github.com/Eduard-L/web__project3_second-version" className='portfolio__link' target='_blank'>HomeLand Code</a>
                         </button>
                     </PortfolioCard>
+                    {
+                        (scrollPosition < 800) && window.innerWidth > 700 &&
+                        <li><button onClick={handleScrollForward} type='button' className={`portfolio__btn portfolio__btn_type_next `}></button></li>
+                    }
 
-                    <li><button onClick={handleScrollForward} type='button' className={`portfolio__btn portfolio__btn_type_next ${scrollPosition >= 850 && 'portfolio__btn_not-visible'}`}></button></li>
+
                 </ul>
             </div>
         </section>
